32a224733c176137adb0df066c5e4d2354b19ec9
Carlos Emil De Vera <cedevera02@gmail.com>
2022-04-30 17:40:05 -0500

    Added table

--- EECS647Project/Pages/FindRecipe.php
+++ EECS647Project/Pages/FindRecipe.php
@@ -1,21 +1,29 @@
-<?php
-$mysqli = new mysqli("mysql.eecs.ku.edu","c952d843","dummypass","c952d843");
+<?php 
+                    $mysqli = new mysqli("mysql.eecs.ku.edu","y283c244","kai9ju3p","y283c244");
 
-if($mysqli->connect_errno){
-     echo "<p>Connection Failed</p>";
-     exit();
-}
+                    if($mysqli->connect_errno){
+                        echo "<p>Connection Failed</p>";
+                        exit();
+                    }
+                    
+                    $query = "SELECT name, type, prep_time, total_price FROM Recipe ORDER BY RAND() LIMIT 1";
 
-$query = "SELECT SHIPNUM FROM SHIP";
+                    if ($result = $mysqli->query($query)) {
+                        /* fetch associative array */
+                            while ($row = $result->fetch_assoc()) {?>
+                               <tr>
+                                    <td><?php echo $row['name'];?></td>
+                                    <td><?php echo $row['type'];?></td>
+                                    <td><?php echo $row['prep_time'];?></td>
+                                    <td><?php echo $row['total_price'];?></td>
+                                    <td>
+                                        <button >Make It!</button>
+                                    </td>
+                               </tr>
 
-
-
-if ($result = $mysqli->query($query)) {
- /* fetch associative array */
-     while ($row = $result->fetch_assoc()) {
-        echo "<"
-     }
- /* free result set */
- $result->free();
-
-?>
+                            <?php }
+                    }
+                        /* free result set */
+                        $result->free();
+                
+                ?>
